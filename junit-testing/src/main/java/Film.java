63d17b5db86ced80f16080fd2caf3fef8357bab7
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-06 13:55:53 +0000

    updating test files

--- junit-testing/src/main/java/Film.java
+++ junit-testing/src/main/java/Film.java
@@ -18,20 +18,46 @@ public class Film {
     public boolean isAllowedToView(int age) {
         return switch (this.rating) {
             case RATING_U -> true;
-            case RATING_PG -> {
-                yield age >= 5;
-            }
-            case RATING_12 -> {
-                yield age >= 12;
-            }
-            case RATING_15 -> {
-                yield age >= 15;
-            }
-            case RATING_18 -> {
-                yield age >= 18;
-            }
+            case RATING_PG -> age >= 5;
+            case RATING_12 -> age >= 12;
+            case RATING_15 -> age >= 15;
+            case RATING_18 -> age >= 18;
             default -> false;
         };
+
+
+//        switch (this.rating) {
+//            case RATING_U:
+//                return true;
+//
+//            case RATING_PG:
+//                if( age >= 5 ) {
+//                    return true;
+//                }
+//                return false;
+//
+//            case RATING_12:
+//                if( age >= 12 ) {
+//                    return true;
+//                }
+//                return false;
+//
+//            case RATING_15:
+//                if( age >= 15 ) {
+//                    return true;
+//                }
+//                return false;
+//
+//            case RATING_18:
+//                if( age >= 18 ) {
+//                    return true;
+//                }
+//                return false;
+//
+//            default:
+//                return false;
+//        }
+//    }
     }
 
 }
