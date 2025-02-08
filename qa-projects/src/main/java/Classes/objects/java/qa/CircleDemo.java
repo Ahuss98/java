3651c575e6a13f9d5a5ad08315f2a44e85961dbf
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-08 16:35:03 +0000

    creates a circle class with a demo class also using string format

---
+++ qa-projects/src/main/java/Classes/objects/java/qa/CircleDemo.java
@@ -0,0 +1,11 @@
+package Classes.objects.java.qa;
+
+public class CircleDemo {
+    public static void main(String[] args){
+        Circle myCircle = new Circle(4);
+        System.out.println(myCircle.getRadius());
+        //using string format to only show 2 decimal indicated by the %.2f in string format
+        System.out.println("area " + String.format("%.2f", myCircle.circumference()));
+        System.out.println(myCircle.area());
+    }
+}
