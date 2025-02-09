665aba0346aadaa43d5058c04163c05ebe424e34
Ahmed Hussain <a.hmedhussain@hotmail.com>
2025-02-09 23:52:19 +0000

    Fixed Enums issue

---
+++ Enums/src/main/java/org/example/DaysOfWeek.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public enum DaysOfWeek {
+    Monday("Monday"),Tuesday("Tuesday"),Wednesday("wednesday"),
+
+    DaysOfWeek(String day){
+
+    }
+}
--- qa-projects/src/main/java/Classes/objects/java/qa/CircleDemo.java
+++ qa-projects/src/main/java/Classes/objects/java/qa/CircleDemo.java
@@ -5,7 +5,8 @@ public class CircleDemo {
         Circle myCircle = new Circle(4);
         System.out.println(myCircle.getRadius());
         //using string format to only show 2 decimal indicated by the %.2f in string format
-        System.out.println("area " + String.format("%.2f", myCircle.circumference()));
-        System.out.println(myCircle.area());
+        System.out.println("circumfrence " + String.format("%.2f", myCircle.circumference()));
+        System.out.println("area "+ String.format("%.2f", myCircle.area()));
+//        System.out.println(myCircle.area());
     }
 }
